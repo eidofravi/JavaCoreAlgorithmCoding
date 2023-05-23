ae8770e26f570b7dd9a32a51c7e4443781f08bfe
ravi <ravi.kh123@gmail.com>
2023-05-22 21:54:19 -0400

    commit

--- src/algorithm/graph/FindTotalPathToDestination2Optimized.java
+++ src/algorithm/graph/FindTotalPathToDestination2Optimized.java
@@ -5,7 +5,7 @@ import algorithm.common.Node;
 import java.util.HashMap;
 import java.util.Map;
 
-public class FindTotalPathToDestination {
+public class FindTotalPathToDestination2Optimized {
 
     // 10, 20, 30
     //40, 50, 60
@@ -26,38 +26,20 @@ public class FindTotalPathToDestination {
         System.out.println(findPath(root, new HashMap()));
         long L2 = System.currentTimeMillis();
         System.out.println("time taken " + (L2 - L1));
-
     }
 
     private static int findPath(Node node, Map<Integer, Integer> map) {
-        int sum = 0;
+        if (node == null) {
+            return 0;
+        }
+        if (node.right == null && node.down == null && node.diagonal == null) {
+            return 1;
+        }
         if (null != map.get(node.value)) {
             return map.get(node.value);
         }
-
-        if (null == node.right && null == node.down && null == node.diagonal) {
-            System.out.println();
-            return 1;
-        }
-
-        if (null != node.right) {
-           // System.out.print(node.right.value + " ");
-            sum += findPath(node.right, map);
-        }
-        if (null != node.down) {
-           // System.out.print(node.down.value + " ");
-            sum += findPath(node.down, map);
-        }
-        if (null != node.diagonal) {
-           // System.out.print(node.diagonal.value + " ");
-            sum += findPath(node.diagonal, map);
-        }
-        System.out.println("===" +  node.value);
-
-        map.put(node.value, sum);
-
-        return sum;
+        int path = findPath(node.right, map) + findPath(node.down, map) + findPath(node.diagonal, map);
+        map.put(node.value, path);
+        return path;
     }
-
-
 }
